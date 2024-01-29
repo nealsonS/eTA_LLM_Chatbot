b7f4ac140ef9354343bc626de3dc4dd64d2b585b
deborahgh <deboragh@usc.edu>
2024-01-28 20:03:36 -0800

    deborah_1/28_update
    deb's half of part 2, as well as sample output files

---
+++ yfinance_scraper.py
@@ -0,0 +1,61 @@
+import yfinance as yf
+import pandas as pd
+from datetime import datetime
+
+
+
+def user_companies():
+	usr_input = input("Enter company name(s), separate by space:\n>>>")
+	companies = usr_input.upper().split(" ")
+	return companies
+	
+	
+def user_period():
+	period_range = ['1d','5d','1mo','3mo','6mo','1y','2y','5y','10y','ytd','max']
+	message = "Enter time range from this list:\n" + str(period_range) + "\n('max' is recommended)\n>>>"
+	usr_input = input(message)
+	if usr_input in period_range:
+		return usr_input
+
+	
+def user_interval():
+	interval_range = ['1m','2m','5m','15m','30m','60m','90m','1h','1d','5d','1wk','1mo','3mo']
+	message = "Enter interval range from this list:\n" + str(interval_range) + "\n>>>"
+	usr_input = input(message)
+	if usr_input in interval_range:
+		return usr_input
+
+	
+#~~~~~~ from https://www.qmr.ai/yfinance-library-the-definitive-guide/#Fetch_Options_Chain_Data_from_Yahoo_Finance  ~~~~~~
+def hist_scraping(companies, period, interval):
+	end_date = datetime.now().strftime('%Y-%m-%d')
+	tickers = yf.Tickers(companies)
+	tickers_hist = tickers.history(period=period, end=end_date, interval=interval)
+	return tickers_hist
+
+
+def export_data(data_hist):
+	data = data_hist
+	print(data.head())
+	df = pd.DataFrame(data)
+	name = input("Name the output file (do not include .csv or .txt):\n>>>")
+	csv_path = "/home/deboragh/Desktop/deborah_2164672036/Lab_3/" + name + ".csv" 
+	df.to_csv(csv_path, index=False)
+	txt_path = "/home/deboragh/Desktop/deborah_2164672036/Lab_3/" + name + ".txt"  #txt for easier loading to mysql
+	df.to_csv(txt_path, sep='\t', index=False)
+	
+	
+	
+#~~~~~~~~~~~~ main() ~~~~~~~~~~~~~~~~~~~
+stock_companies = user_companies()
+print(stock_companies)
+stock_period = user_period()
+print(stock_period)
+stock_interval = user_interval()
+print(stock_interval)
+
+data = hist_scraping(stock_companies, stock_period, stock_interval)
+export_data(data)
+	
+	
+
