e5ff693a645974cfcd8b57eebd9556c482e77cb6
ColinZWang <134334380+ColinZWang@users.noreply.github.com>
2024-02-12 17:18:37 -0800

    Update keyword_extraction.py

--- lab4_p1/keyword_extraction.py
+++ lab4_p1/keyword_extraction.py
@@ -36,14 +36,37 @@ def preprocess_text(text):
     text = re.sub(r'[^a-zA-Z0-9\s]', '', text)  # Remove special characters
     return text
 
+# NLTK keyword extraction 
+# def extract_keywords(text):
+#     """
+#     Extracts keywords from text using NLTK for tokenization and stopword removal.
+#     """
+#     stop_words = set(stopwords.words('english'))
+#     word_tokens = word_tokenize(text)
+#     keywords = [word for word in word_tokens if word not in stop_words and word.isalpha()]
+#     return ', '.join(keywords)
+
+# Default Rake
+# def extract_keywords(text):
+#     # Initialize RAKE by providing a list of stopwords
+#     rake = RAKE.Rake(RAKE.SmartStopList())
+    
+#     # Extract keywords. The run method returns a list of tuples (keyword, score)
+#     keywords = rake.run(text)
+    
+#     # For simplicity, let's return only keywords, not scores, and join them into a string
+#     keywords = ', '.join([keyword for keyword, score in keywords])
+#     return keywords
+
+# From rake.py
 def extract_keywords(text):
-    """
-    Extracts keywords from text using NLTK for tokenization and stopword removal.
-    """
-    stop_words = set(stopwords.words('english'))
-    word_tokens = word_tokenize(text)
-    keywords = [word for word in word_tokens if word not in stop_words and word.isalpha()]
-    return ', '.join(keywords)
+    r = Rake()  # Initialize RAKE
+    r.extract_keywords_from_text(text)
+    keyword_phrases = r.get_ranked_phrases()  # Extract keyword phrases ranked by relevance
+    keywords = ', '.join(keyword_phrases)  # Join phrases into a single string
+    return keywords
+
+
 
 def get_user_password():
     user = input("Please enter your MySQL user:\nLeave Blank if it is root\n")
