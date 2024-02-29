2def1fb12ed375051e0a450c4373638d5f134f77
deborahgho <vboxuser@ubuntu-2024-more.myguest.virtualbox.org>
2024-02-29 12:45:30 -0800

    update my-app content

--- lab5_p2/my-app/main.js
+++ lab5_p2/my-app/main.js
@@ -1,87 +1,91 @@
-//DEFAULT CODE
-//import './style.css';
-//import {Map, View} from 'ol';
-//import TileLayer from 'ol/layer/Tile';
-//import OSM from 'ol/source/OSM';
+import {Feature, Map, Overlay, View} from 'ol/index.js';
+import {OSM, Vector as VectorSource} from 'ol/source.js';
+import {Point} from 'ol/geom.js';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
+import {useGeographic} from 'ol/proj.js';
 
-//const map = new Map({
-//target: 'map',
-//layers: [
-//  new TileLayer({
-//    source: new OSM()
-//  })
-//],
-//  view: new View({
-//    center: [0, 0],
-//    zoom: 2
-//  })
-//});
+useGeographic();
 
+const place = [-110, 45];
 
-// code from https://openlayers.org/en/v8.1.0/examples/overlay.html
-
-import Map from 'ol/Map.js'; // all of these are in the my-app folder... not sure why the pin is not appearing
-import OSM from 'ol/source/OSM.js';
-import Overlay from 'ol/Overlay.js';
-import TileLayer from 'ol/layer/Tile.js';
-import View from 'ol/View.js';
-import {fromLonLat, toLonLat} from 'ol/proj.js';
-import {toStringHDMS} from 'ol/coordinate.js';
-
-const layer = new TileLayer({
-  source: new OSM(),
-});
+const point = new Point(place);
 
 const map = new Map({
-  layers: [layer],
   target: 'map',
   view: new View({
-    center: [0, 0],
-    zoom: 2,
+    center: place,
+    zoom: 8,
   }),
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+    new VectorLayer({
+      source: new VectorSource({
+        features: [new Feature(point)],
+      }),
+      style: {
+        'circle-radius': 9,
+        'circle-fill-color': 'red',
+      },
+    }),
+  ],
 });
 
-const pos = fromLonLat([16.3725, 48.208889]);
+const element = document.getElementById('popup');
 
-// Popup showing the position the user clicked
 const popup = new Overlay({
-  element: document.getElementById('popup'),
+  element: element,
+  stopEvent: false,
 });
 map.addOverlay(popup);
 
-// Vienna marker
-const marker = new Overlay({
-  position: pos,
-  positioning: 'center-center',
-  element: document.getElementById('marker'),
-  stopEvent: false,
-});
-map.addOverlay(marker);
+function formatCoordinate(coordinate) {
+  return `
+    <table>
+      <tbody>
+        <tr><th>lon</th><td>${coordinate[0].toFixed(2)}</td></tr>
+        <tr><th>lat</th><td>${coordinate[1].toFixed(2)}</td></tr>
+      </tbody>
+    </table>`;
+}
 
-// Vienna label
-const vienna = new Overlay({
-  position: pos,
-  element: document.getElementById('vienna'),
+const info = document.getElementById('info');
+map.on('moveend', function () {
+  const view = map.getView();
+  const center = view.getCenter();
+  info.innerHTML = formatCoordinate(center);
 });
-map.addOverlay(vienna);
 
-const element = popup.getElement();
-map.on('click', function (evt) {
-  const coordinate = evt.coordinate;
-  const hdms = toStringHDMS(toLonLat(coordinate));
-  popup.setPosition(coordinate);
-  let popover = bootstrap.Popover.getInstance(element);
+let popover;
+map.on('click', function (event) {
   if (popover) {
     popover.dispose();
+    popover = undefined;
   }
+  const feature = map.getFeaturesAtPixel(event.pixel)[0];
+  if (!feature) {
+    return;
+  }
+  const coordinate = feature.getGeometry().getCoordinates();
+  popup.setPosition([
+    coordinate[0] + Math.round(event.coordinate[0] / 360) * 360,
+    coordinate[1],
+  ]);
+
   popover = new bootstrap.Popover(element, {
-    animation: false,
-    container: element,
-    content: '<p>The location you clicked was:</p><code>' + hdms + '</code>',
+    container: element.parentElement,
+    content: formatCoordinate(coordinate),
     html: true,
+    offset: [0, 20],
     placement: 'top',
-    title: 'Welcome to OpenLayers',
+    sanitize: false,
   });
   popover.show();
+});
+
+map.on('pointermove', function (event) {
+  const type = map.hasFeatureAtPixel(event.pixel) ? 'pointer' : 'inherit';
+  map.getViewport().style.cursor = type;
 });
 
