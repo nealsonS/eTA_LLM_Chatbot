f86f89ac73746d9261910fe650eec595fdde51cc
ColinZWang <wangzixi@usc.edu>
2024-03-04 17:46:20 -0800

    colin's version attempt

--- lab5_p2/col_ver/vite.config.js
+++ lab5_p2/col_ver/vite.config.js
@@ -1,11 +1,5 @@
-import { defineConfig } from 'vite';
-import nodePolyfills from 'rollup-plugin-polyfill-node';
-
-export default defineConfig({
-  plugins: [nodePolyfills()],
-  resolve: {
-    alias: {
-      'buffer': 'buffer/'
-    }
+export default {
+  build: {
+    sourcemap: true,
   }
-});
+}
