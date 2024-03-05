5b1660192ffe37f4320a6b4997e42fd06152db79
ColinZWang <134334380+ColinZWang@users.noreply.github.com>
2024-03-04 22:32:51 -0800

    Update v6_map.js

--- lab5_p2/google_maps/v6_map.js
+++ lab5_p2/google_maps/v6_map.js
@@ -85,6 +85,148 @@ function buildContent(place) {
   return content;
 }
 
+const new_places = [
+  {
+    address: '1001 Fannin,Sulte 1 500',
+    well_name: 'Lewis Federal 5300 11-31 3B',
+    well_status: 'Active',
+    well_type: 'Oil & Gas',
+    closest_city: 'Williston',
+    barrels_of_oil: '1.9 k',
+    barrels_of_gas: '5.7 k',
+    position: { lat: 48.03581, lng: 103.602608 },
+  },
+  {
+    address: 'D Drilling Prognosis \n' +
+      'D Redrilling or Repair \n' +
+      'D Casing or Liner \n' +
+      'D PlugWell \n' +
+      'D Supplemental History \n' +
+      'D Temporarily Abandon \n' +
+      'D Other \n' +
+      'I Township \n' +
+      '153 N I Range \n' +
+      '101 w \n' +
+      'I County \n' +
+      'Williams \n' +
+      'lCity \n' +
+      'DETAILS OF WORK D \n' +
+      'D',
+    well_name: 'Atlanta 3-6H',
+    well_status: 'Active',
+    well_type: 'Oil & Gas',
+    closest_city: 'Williston',
+    barrels_of_oil: '429',
+    barrels_of_gas: '725',
+    position: { lat: 48.1094, lng: -103.7312 },
+  },
+  {
+    address: 'I I I \nI I I \nI',
+    well_name: 'Atlanta 4-6H',
+    well_status: 'Active',
+    well_type: 'Oil & Gas',
+    closest_city: 'Williston',
+    barrels_of_oil: '348',
+    barrels_of_gas: '627',
+    position: { lat: 48.1094, lng: -103.7329 },
+  },
+  {
+    address: 'on the reverse \nso that we can return the card to you',
+    well_name: 'Buck Shot SWD 5300 31-31',
+    well_status: 'Active',
+    well_type: 'Salt Water Disposal',
+    closest_city: 'Williston',
+    barrels_of_oil: 'Not available',
+    barrels_of_gas: 'Not available',
+    position: { lat: 48.02, lng: 103.35 },
+  },
+  {
+    address: 'D \n' +
+      'D \n' +
+      'D Casing or Liner \n' +
+      'D PlugWell \n' +
+      'D Supplemental History \n' +
+      'D Temporarily Abandon \n' +
+      '0 Other \n' +
+      'I Township \n' +
+      '153 N I Range \n' +
+      '101 w \n' +
+      'I County \n' +
+      'Williams \n' +
+      'lCity \n' +
+      'DETAILS OF WORK Oil \n' +
+      'Water \n' +
+      'Gas',
+    well_name: 'Atlanta 11-6H',
+    well_status: 'Active',
+    well_type: 'Oil & Gas',
+    closest_city: 'Williston',
+    barrels_of_oil: '731',
+    barrels_of_gas: '1.1 k',
+    position: { lat: 48.11, lng: -103.73 },
+  },
+  {
+    address: 'O Drilling Prognosis \n' +
+      '0 Redrilling or Repair \n' +
+      '0 Casing or Liner \n' +
+      'O PlugWell \n' +
+      'O Supplemental History \n' +
+      'D Temporarily Abandon \n' +
+      '0 Other \n' +
+      'I Township \n' +
+      '153 N I Range \n' +
+      '101 w I County \n' +
+      'Williams \n' +
+      'I City \n' +
+      'DETAILS OF WORK Oil \n' +
+      'Water \n' +
+      'Gas D \n' +
+      'D',
+    well_name: 'Atlanta 12-6H',
+    well_status: 'Active',
+    well_type: 'Oil & Gas',
+    closest_city: 'Williston',
+    barrels_of_oil: '236',
+    barrels_of_gas: '493',
+    position: { lat: 48.109, lng: -103.728 },
+  },
+  {
+    address: '',
+    well_name: 'Atlanta 1-6H',
+    well_status: 'Active',
+    well_type: 'Oil & Gas',
+    closest_city: 'Williston',
+    barrels_of_oil: '947',
+    barrels_of_gas: '1.4 k',
+    position: { lat: 48.11, lng: -103.73 },
+  },
+  {
+    address: 'D \n' +
+      'D Redrilling or Repair \n' +
+      'D Casing or Liner \n' +
+      'D Plug Well \n' +
+      'D Supplemental History \n' +
+      'D Temporarily Abandon \n' +
+      'D Other \n' +
+      'I Township \n' +
+      '153 N I Range \n' +
+      '101 w I County \n' +
+      'Williams \n' +
+      'DETAILS OF WORK Oil \n' +
+      'Water \n' +
+      'Gas D \n' +
+      'D IWell File No',
+    well_name: 'Atlanta 13-6H',
+    well_status: 'Active',
+    well_type: 'Oil & Gas',
+    closest_city: 'Williston',
+    barrels_of_oil: '708',
+    barrels_of_gas: '1.1 k',
+    position: { lat: 48.109, lng: -103.728 },
+  },
+];
+
+
 const places1 = [
   {
     address: "address",
