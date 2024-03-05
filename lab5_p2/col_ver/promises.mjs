7f43deb354033d94904d832dde7c8beb70e7477b
ColinZWang <wangzixi@usc.edu>
2024-03-04 17:23:46 -0800

    colin's version attempt

---
+++ lab5_p2/col_ver/promises.mjs
@@ -0,0 +1,36 @@
+import mysql from 'mysql2/promise'; 
+import util from 'util';
+
+const dbConfig = {
+    host: 'localhost',
+    user: 'root',
+    //password: '',
+    database: 'demo1'
+};
+
+const connection = mysql.createConnection(dbConfig);
+const query = util.promisify(connection.query).bind(connection);
+
+const fetchDataFromDB = async () => {
+    let well_info = [];
+    try {
+        await connection.connect();
+        const query2 = "SELECT longitude, latitude FROM well_data WHERE longitude != 'Unknown' and latitude != 'Unknown'";
+        const results = await query(query2);
+
+        results.forEach(row => {
+            well_info.push(row.longitude, row.latitude);
+        });
+
+        //console.log("results from promises", results);
+        //console.log("well_info from promises", well_info);
+        return well_info;
+
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    } finally {
+        await connection.end();
+    }
+};
+
+export default { fetchDataFromDB };
---
+++ lab5_p2/col_ver/readme.md
@@ -0,0 +1,36 @@
+# OpenLayers + Vite
+
+This app is modified from the following example, it demonstrates how the `ol` package can be used with [Vite](https://vitejs.dev/).
+
+To get started, install node.js:
+
+    sudo apt update
+    sudo apt install nodejs
+Check that the install was successful by querying node for its version number:
+
+    node -v
+
+Install npm:
+
+    sudo apt install npm
+
+OpenLayers require node version of 14.0 or later, therefore we update npm:
+
+    sudo npm cache clean -f
+    sudo npm install -g n
+    sudo n stable
+
+Install necessary dependencies:
+
+    npm install
+    
+Then change into your new `my-app` directory and start a development server (available at http://localhost:5173):
+
+    cd my-app
+    npm start
+
+To generate a build ready for production:
+
+    npm run build
+
+Then deploy the contents of the `dist` directory to your server.  You can also run `npm run serve` to serve the results of the `dist` directory for preview.
