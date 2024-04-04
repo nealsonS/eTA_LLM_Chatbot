24ed42eebd8cf8f9759568ae3b3f402cc2b3b476
deborahgho <vboxuser@ubuntu-2024-more.myguest.virtualbox.org>
2024-04-03 17:42:49 -0700

    update?

--- chatbot/video/test/Word.py
+++ chatbot/video/test/Word.py
@@ -8,15 +8,20 @@ class Word:
         Parameters:
           dict (dict) dictionary from JSON, containing:
             word (str): recognized word
+            text (str): recognized word
             start (float): start time of the pronouncing the word, in seconds
             end (float): end time of the pronouncing the word, in seconds
+            duration (float): total time of pronouncing the word, in seconds (end - start)
         '''
         self.word = dict["word"]
+        self.text = dict["word"]
         self.end = dict["end"]
         self.start = dict["start"]
+        self.duration = dict["end"] - dict["start"]
 
 
     def to_string(self):
         ''' Returns a string describing this instance '''
-        return "{:20} from {:.2f} sec to {:.2f} sec".format(
-            self.word, self.start, self.end)
+        return {'text': self.text, 'start': self.start, 'duration': self.duration}
+        #return "{:20} starting from {:.2f} sec taking {:.2f} secs".format(
+        #    self.text, self.start, self.duration)
