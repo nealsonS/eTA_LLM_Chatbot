1f08c3f6154d0816516e7fdeb9f7002c930f71a3
deborahgho <vboxuser@ubuntu-2024-more.myguest.virtualbox.org>
2024-04-13 15:02:28 -0700

    worked on rag_chain a bit

---
+++ chatbot/Milvus/rag_chain_v2.py
@@ -0,0 +1,42 @@
+from langchain.vectorstores.milvus import Milvus
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain.schema.runnable import RunnablePassthrough
+
+if __name__ == '__main__':
+
+	COLLECTION_NAME = 'db_560'
+	host = 'localhost'
+	port = '19530'
+	URI = f'http://{host}:{port}' # connection address for milvus
+	connection_args = { 'uri': URI }
+
+	embeddings = HuggingFaceEmbeddings()
+	retriever = Milvus(
+		embedding_function = embeddings,
+		connection_args=connection_args,
+		collection_name = COLLECTION_NAME
+	).as_retriever()
+
+	llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0) 
+
+
+	# code from https://milvus.io/docs/integrate_with_langchain.md
+	template = """Use the following pieces of context to answer the question at the end. 
+If you don't know the answer, just say that you don't know, don't try to make up an answer. 
+Use three sentences maximum and keep the answer as concise as possible. 
+Always say "thanks for asking!" at the end of the answer. 
+{context}
+Question: {question}
+Helpful Answer:"""
+
+	rag_prompt = PromptTemplate.from_template(template)
+
+rag_chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | rag_prompt
+    | llm
+)
+
+print(rag_chain.invoke("What are quality assessments for drug therapy?"))
