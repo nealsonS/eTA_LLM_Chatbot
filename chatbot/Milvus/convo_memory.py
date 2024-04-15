821bc84b4828dd2de3209f96a2f00dfd89ebbbd6
deborahgho <vboxuser@ubuntu-2024-more.myguest.virtualbox.org>
2024-04-14 18:42:37 -0700

    rag_chain 3

--- chatbot/Milvus/convo_memory.py
+++ chatbot/Milvus/convo_memory.py
@@ -20,16 +20,17 @@ if __name__ == '__main__':
 	).as_retriever()
 
 	#llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0) 
-	llm = ChatOpenAI(model_name="gpt-4", temperature=0.0) 
+	llm = ChatOpenAI(model_name="gpt-4", temperature=0) 
 
 
 	# code from https://milvus.io/docs/integrate_with_langchain.md
 	template = """Use the following pieces of context to answer the question at the end. 
-	If you don't know the answer, just say that you don't know, don't try to make up an answer. 
-	Use three sentences maximum and keep the answer as concise as possible. 
-	{context}
-	Question: {question}
-	Helpful Answer:"""
+If you don't know the answer, just say that you don't know, don't try to make up an answer. 
+Use three sentences maximum and keep the answer as concise as possible. 
+Always say "thanks for asking!" at the end of the answer. 
+{context}
+Question: {question}
+Helpful Answer:"""
 
 	rag_prompt = PromptTemplate.from_template(template)
 
@@ -39,15 +40,7 @@ if __name__ == '__main__':
 	    | llm
 	)
 
-	while True:
-		question = input("YOU: ")
-		#question = "What are quality assessments for drug therapy?"
-		if question.lower() == 'quit':
-			print("\nCHATBOT: Good luck!")
-			break
-		output = rag_chain.invoke(question)
-		print("\nCHATBOT:", output.content, "\n")
-    
-
-
-
+	question = input("YOU: ")
+	#question = "What are quality assessments for drug therapy?"
+	output = rag_chain.invoke(question)
+	print("\nCHATBOT:", output.content, "\n")
